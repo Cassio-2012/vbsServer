54b6e5eca7243282aa007d98b7e516da48e376ff
Ubuntu <ubuntu@ip-172-31-39-131.ec2.internal>
2020-10-17 23:33:30 +0000

    ajustes tratamento de dados

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -10,11 +11,19 @@ app.use(cors());
 
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-app.post('/data', function(req, res){
-    var data = req.body;
-    console.log(data);
-    return res.send('[+] END')
-});
+  
+app.post('/data', function(req, res, params){
+  
+  var body = '';
+  req.on('data', function (data) {
+      body += data;
+  });
+  req.on('end', function () {
+      console.log(body)     
+  });
 
+  return res.send('[+] END')
+
+});
 
 app.listen(9001, () => console.log('Express started at http://localhost:9001'));
